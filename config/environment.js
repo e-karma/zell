33b54fd291a5b33504409eeb1e7f7f758ccbc9ea
e-karma <djbradyla@gmail.com>
2015-01-16 15:05:58 -0800

    first

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,8 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'zell',
+    podModulePrefix: 'zell/pods',
+    usePodsByDefault: true,
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
